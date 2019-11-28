7a6e6dcdae74b398daf7d613fe1e1b9d9d1dc2ea
fangx <fangxin9036@163.com>
2019-11-28 11:31:03 +0800

    注释部分swagger

--- src/main/java/io/geekidea/springbootplus/common/service/impl/BaseServiceImpl.java
+++ src/main/java/io/geekidea/springbootplus/common/service/impl/BaseServiceImpl.java
@@ -20,9 +20,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import io.geekidea.springbootplus.common.service.BaseService;
 import io.geekidea.springbootplus.common.param.OrderQueryParam;
 import io.geekidea.springbootplus.common.param.QueryParam;
+import io.geekidea.springbootplus.common.service.BaseService;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.Arrays;
@@ -35,10 +35,10 @@ import java.util.List;
 public abstract class BaseServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements BaseService<T> {
 
     protected Page setPageParam(QueryParam queryParam) {
-        return setPageParam(queryParam,null);
+        return setPageParam(queryParam, null);
     }
 
-    protected Page setPageParam(QueryParam queryParam, OrderItem defaultOrder) {
+    protected Page<T> setPageParam(QueryParam queryParam, OrderItem defaultOrder) {
         Page page = new Page();
         // 设置当前页码
         page.setCurrent(queryParam.getCurrent());
@@ -48,15 +48,15 @@ public abstract class BaseServiceImpl<M extends BaseMapper<T>, T> extends Servic
          * 如果是queryParam是OrderQueryParam，并且不为空，则使用前端排序
          * 否则使用默认排序
          */
-        if (queryParam instanceof OrderQueryParam){
+        if (queryParam instanceof OrderQueryParam) {
             OrderQueryParam orderQueryParam = (OrderQueryParam) queryParam;
             List<OrderItem> orderItems = orderQueryParam.getOrders();
-            if (CollectionUtils.isEmpty(orderItems)){
+            if (CollectionUtils.isEmpty(orderItems)) {
                 page.setOrders(Arrays.asList(defaultOrder));
-            }else{
+            } else {
                 page.setOrders(orderItems);
             }
-        }else{
+        } else {
             page.setOrders(Arrays.asList(defaultOrder));
         }
 
